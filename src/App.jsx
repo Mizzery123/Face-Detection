1899e99d23abce87ed950f103adefa795136cd4e
17000719 <17000719@myrp.edu.sg>
2022-01-26 21:13:35 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import "./app.css";
+import Navbar from "./components/Navbar";
+import NewPost from "./components/NewPost";
+
+function App() {
+  const [file, setFile] = useState();
+  const [image, setImage] = useState();
+
+  useEffect(() => {
+    const getImage = () => {
+      const img = new Image();
+      img.src = URL.createObjectURL(file);
+      img.onload = () => {
+        setImage({
+          url: img.src,
+          width: img.width,
+          height: img.height,
+        });
+      };
+    };
+
+    file && getImage();
+  }, [file]);
+
+  return (
+    <div>
+      <Navbar />
+      {image ? (
+        <NewPost image={image} />
+      ) : (
+        <div className="newPostCard">
+          <div className="addPost">
+            <img
+              src="https://img.freepik.com/free-photo/profile-portrait-gorgeous-cute-handsome-young-man-with-perfect-features-bristle-reddish-hair-posing-blank-copyspace-wall-jumper-looking-up-with-inspired-dreamy-facial-expression_343059-2574.jpg?size=626&ext=jpg"
+              alt=""
+              className="avatar"
+            />
+            <div className="postForm">
+              <input
+                type="text"
+                placeholder="What's on your mind?"
+                className="postInput"
+              />
+              <label htmlFor="file">
+                <img
+                  className="addImg"
+                  src="https://cdn.icon-icons.com/icons2/564/PNG/512/Add_Image_icon-icons.com_54218.png"
+                  alt=""
+                />
+                <img
+                  className="addImg"
+                  src="https://icon-library.com/images/maps-icon-png/maps-icon-png-5.jpg"
+                  alt=""
+                />
+                <img
+                  className="addImg"
+                  src="https://d29fhpw069ctt2.cloudfront.net/icon/image/84451/preview.svg"
+                  alt=""
+                />
+                <button>Send</button>
+              </label>
+              <input
+                onChange={(e) => setFile(e.target.files[0])}
+                id="file"
+                style={{ display: "none" }}
+                type="file"
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
